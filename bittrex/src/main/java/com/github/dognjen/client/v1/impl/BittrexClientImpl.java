f9e89348357a902fa1dcf8b7da9c2f138f456972
Dejan Ognjenović <dejan.ognjenovic@sunesis.si>
2017-11-12 22:23:46 +0100

    /

---
+++ bittrex/src/main/java/com/github/dognjen/client/v1/impl/BittrexClientImpl.java
@@ -0,0 +1,85 @@
+package com.github.dognjen.client.v1.impl;
+
+import com.github.dognjen.client.v1.BittrexClient;
+
+/**
+ * Created by Dejan Ognjenović on 12/11/2017.
+ */
+public class BittrexClientImpl implements BittrexClient {
+
+    public void getMarkets() {
+
+    }
+
+    public void getCurrencies() {
+
+    }
+
+    public void getTicker() {
+
+    }
+
+    public void getMarketSummaries() {
+
+    }
+
+    public void getMarketSummary() {
+
+    }
+
+    public void getOrderBook() {
+
+    }
+
+    public void getMarketHistory() {
+
+    }
+
+    public void buyLimit() {
+
+    }
+
+    public void sellLimit() {
+
+    }
+
+    public void cancel() {
+
+    }
+
+    public void getOpenOrders() {
+
+    }
+
+    public void getBalances() {
+
+    }
+
+    public void getBalance() {
+
+    }
+
+    public void getDepositAddress() {
+
+    }
+
+    public void withdraw() {
+
+    }
+
+    public void getOrder() {
+
+    }
+
+    public void getOrderHistory() {
+
+    }
+
+    public void getWithdrawalHistory() {
+
+    }
+
+    public void getDepositHistory() {
+
+    }
+}

fafab9f427d2fe4da0de245b9c83d85c8c08465f
Dejan Ognjenović <dognjen@yandex.com>
2017-11-25 18:03:06 +0100

    Changes in BittrexClient / Not  done yet

--- bittrex/src/main/java/com/github/dognjen/client/v1/impl/BittrexClientImpl.java
+++ bittrex/src/main/java/com/github/dognjen/client/v1/impl/BittrexClientImpl.java
@@ -1,13 +1,43 @@
 package com.github.dognjen.client.v1.impl;
 
 import com.github.dognjen.client.v1.BittrexClient;
+import com.github.dognjen.client.v1.utils.ClientUtils;
+import com.github.dognjen.client.v1.utils.properties.BittrexProperties;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by Dejan Ognjenović on 12/11/2017.
  */
+@RequestScoped
 public class BittrexClientImpl implements BittrexClient {
 
+    @Inject
+    Client client;
+
+    @Inject
+    BittrexProperties properties;
+
+    private WebTarget target;
+
+    @PostConstruct
+    private void init() {
+
+        // TODO if url in config
+        target = client.target(ClientUtils.BITTREX_REST_ENDPOINT)
+                        .queryParam("apikey", properties.getBittrexApikey())
+                        .queryParam("nonce", properties.getBittrexNonce());
+    }
+
     public void getMarkets() {
+
+        target.request(MediaType.APPLICATION_JSON_TYPE).get();
+
 
     }
 

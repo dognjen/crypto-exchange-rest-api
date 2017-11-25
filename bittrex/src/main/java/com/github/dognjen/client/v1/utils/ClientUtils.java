b396a1e57fef218fcefc8dfca54a2060a67f6fba
Dejan Ognjenović <dognjen@yandex.com>
2017-11-25 18:00:39 +0100

    Added ClientUtils

---
+++ bittrex/src/main/java/com/github/dognjen/client/v1/utils/ClientUtils.java
@@ -0,0 +1,11 @@
+package com.github.dognjen.client.v1.utils;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * Created by Dejan Ognjenović on 25/11/2017.
+ */
+public class ClientUtils {
+
+    public static final String BITTREX_REST_ENDPOINT = "";
+}

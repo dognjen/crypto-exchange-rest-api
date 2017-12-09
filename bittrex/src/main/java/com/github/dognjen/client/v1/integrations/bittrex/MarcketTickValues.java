240aead08df2b1b19a72e79d898958752bb7b356
Dejan Ognjenović <dognjen@yandex.com>
2017-12-09 22:27:05 +0100

    Added parameters.

--- bittrex/src/main/java/com/github/dognjen/client/v1/integrations/bittrex/MarcketTickValues.java
+++ bittrex/src/main/java/com/github/dognjen/client/v1/integrations/bittrex/MarcketTickValues.java
@@ -1,7 +1,37 @@
 package com.github.dognjen.client.v1.integrations.bittrex;
 
 /**
- * Created by Dejan on 09/12/2017.
+ * Created by Dejan Ognjenović on 09/12/2017.
  */
 public class MarcketTickValues {
+
+    private Double bid;
+
+    private Double ask;
+
+    private Double last;
+
+    public Double getBid() {
+        return bid;
+    }
+
+    public void setBid(Double bid) {
+        this.bid = bid;
+    }
+
+    public Double getAsk() {
+        return ask;
+    }
+
+    public void setAsk(Double ask) {
+        this.ask = ask;
+    }
+
+    public Double getLast() {
+        return last;
+    }
+
+    public void setLast(Double last) {
+        this.last = last;
+    }
 }
